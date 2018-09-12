95d2425cd0ba99ec3f9ee692e208a1b596b35ebf
npelkone <niko.pelkonen@hotmail.com>
2018-09-12 18:07:23 +0300

    lab1

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,36 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		
+		play.playerBTurn(1);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(1);
+		System.out.println(play.getScore());
+		
+		play.playerBTurn(2);
+		System.out.println(play.getScore());
+		
+		play.playerBTurn(3);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(2);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(3);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(4);
+		System.out.println(play.getScore());
+		
+		play.playerBTurn(4);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(5);
+		System.out.println(play.getScore());
+		
+		play.playerATurn(6);
+		System.out.println(play.getScore());
 	}
 
 }
