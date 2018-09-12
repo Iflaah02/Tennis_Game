d7a112024ee74b2a4716ff6c7e5a1e510f18054e
Juho Jokinen <kohojuntti@gmail.com>
2018-09-12 18:12:39 +0300

    Tennis game completed

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,24 @@
+import java.util.Random;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		
+		int result;
+		int gameOver = 0;
+		
+		Random rand = new Random();
+		
+		while(gameOver == 0) {
+			
+			result = rand.nextInt(2) + 1;
+			
+			gameOver = play.getScore(result);
+			System.out.printf("\n");
+			
+		}
 	}
 
 }
