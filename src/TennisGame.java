8b784b421005d0db0cf81407cd649c7f5e343f44
ilundbom <ilundbom@W10-156.student.yo.oulu.fi>
2018-09-12 18:03:20 +0300

    First try

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,34 +1,109 @@
+import java.io.*;
 
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
 	private int scoreFlag;
+	private String score;
+	private boolean adv1;
+	private boolean adv2;
 	
-	public TennisGame() {
+	public TennisGame(){
 		// TO BE IMPLEMENTED
 		System.out.println("I am constructor");
+		
+        //InputStreamReader in = new InputStreamReader(System.in);
+        //BufferedReader br = new BufferedReader(in);		
+		
+		
+		//System.out.println("Point to:");
+		//String point=br.readLine();
+		
+		p1 = 40;
+		p2 = 15;
+		p2 = 30;
+		p1 = 40;
+		p2 = 40;
+		//p2 = 40;
+		//p1 = 50;
+		//p2 = 50;
 		
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
+		//System.out.println("I am method for displaying score");
 		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
-		// "15 - 15"
+		
+		//draws
+		if (p1==0 && p2==0)
+			score = "love-love)";
+		else if (p1==15 && p2==15)
+			score = "fifteen-fifteen";
+		else if (p1==30 && p2==30)
+			score = "thirteen-thirteen";
+		else if (p1==40 && p2==40)
+			score = "forty-forty";
+		else if (p1 == p2)
+			score = "deuce";
+		
 		// "30 - 30"
 		// "deuce"
+		
+		//leading
+		if (p1 == 15 && p2 == 0 || p1 == 0 && p2 == 15)
+			score = (p1 + "-" + p2);
+		
 		// "15 - 0", "0 - 15"
+
+		if (p1 == 30 && p2 == 0 || p1 == 0 && p2 == 30)
+			score = (p1 + "-" + p2);		
+		
 		// "30 - 0", "0 - 30"
+
+		if (p1 == 40 && p2 == 0 || p1 == 0 && p2 == 40)
+			score = (p1 + "-" + p2);
+		
+		if (p1 > 40 && p2 < 40)
+			score = ("game player1");
+		
+		if (p2 > 40 && p1 < 40)
+			score = ("game player2");
+		
+		
 		// "40 - 0", "0 - 40"
+
+		
+		if (p1 == 30 && p2 == 15 || p1 == 15 && p2 == 30)
+			score = (p1 + "-" + p2);		
 		// "30 - 15", "15 - 30"
+
+		if (p1 == 40 && p2 == 15 || p1 == 15 && p2 == 40)
+			score = (p1 + "-" + p2);		
 		// "40 - 15", "15 - 40"
+
+		if (p1 == 40 && p2 == 30 || p1 == 30 && p2 == 40)
+			score = (p1 + "-" + p2);		
+		
+		//else if (p2 <= 40 && p1 < 40)
+		//	score = (p1 + "-" + p2);
+		
+		//else if (p1 == 40 && p2 == 40)
+		//	score = ("deuce");
+		
+		//else if (p1 > 40 && p2 == 40)
+		//	score = ("advantage player 1");
+
+		//else if (p2 > 40 && p1 == 40)
+		//	score = ("advantage player 2");				
+				
 		// "advantage player1"
 		// "advantage player2"
 		// "game player1"
 		// "game player2"
 
 		// TO BE IMPLEMENTED
-		return "";
+		return score;
 	}
+
 }
