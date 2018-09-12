eb5f48e353162691ea31a8f2c877a467bec99b6e
Sami Frogell <Dalzedur@gmail.com>
2018-09-12 18:15:32 +0300

    Assignment finished, I think

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		//play.getScore();
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,18 +1,120 @@
+import java.util.Random;
+
+//Solution to assignment 1 of SQaT lab#1, Frogell Sami 1866421, Dalzedur@gmail.com
 
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
-	private int scoreFlag;
+	private int difference;
+	private String score;
+	private int round;
 	
-	public TennisGame() {
-		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
+	public TennisGame() 
+	{
+	 round = 0;		 
+//plays until either player has 4 score and difference is at least 2
+		while( ((p1 < 4 || p2 < 4)) || difference < 2  
+             )
+		{
+		 //a bit kludge, but hey, it works!	
+		 if(score == "Player one wins!" || score == "Player two wins!")
+		 break;
+		 round++;
+		 System.out.println("---\nRound "+round);
+		 Random ran = new Random();
+		 int x = ran.nextInt(2);
+		 
+         if(x == 0)
+         {
+          p1++;
+          System.out.println("Player one scores.");
+         }
+         else
+         {
+          p2++;
+          System.out.println("Player two scores.");
+         }
+
+         if(p1 > p2)
+             difference = p1-p2;
+             else
+             difference = p2-p1;         
+         
+         //System.out.println("Debug: P1:"+p1+",P2:"+p2+", Difference is "+difference);
+         getScore();
+		}
+		//System.out.println("I am constructor");
 		
 	}
 
-	public String getScore() {
-		System.out.println("I am method for displaying score");
+	public String getScore() 
+	{
+	 //System.out.println("Debug: getScore");	
+	 score = "";
+	 switch(p1)
+	 {
+	 case 0:
+	 score += "love";
+	 break;
+	 case 1:
+	 score += "fifteen";
+	 break;
+	 case 2:
+	 score += "thirty";
+	 break;
+	 case 3:
+	 score += "forty";		 
+	 break;
+	 default:
+	 //score += "P1 Advantage";		 
+	 break;
+	 }
+	 
+	 score += " | ";
+	 
+	 switch(p2)
+	 {
+	 case 0:
+	 score += "love";
+	 break;
+	 case 1:
+	 score += "fifteen";
+	 break;
+	 case 2:
+	 score += "thirty";
+	 break;
+	 case 3:
+	 score += "forty";		 
+	 break;
+	 default:
+	 //score += "P2 Advantage";		 
+	 break;
+	 }
+	 
+	 if(p1 > 2 && p2 > 2 && p1 == p2)
+	 score = "Deuce";
+	 
+	 if( (p1 > 2 && p2 > 2) && p1 != p2 )
+	 {
+	  if(p1 > p2)
+	  score = "Player one advantage";
+	  else
+	  score = "Player two advantage";
+	 }
+     
+     if(p1 > p2 && (p1 > 3 && difference > 1) )
+     {
+      score = "Player one wins!";	 
+     }
+     else if(p2 > p1 && (p2 > 3 && difference > 1) )
+     {
+      score = "Player two wins!";	 
+     }
+	 
+     System.out.println("Round "+round+" result: "+score);
+	 
+		//System.out.println("I am method for displaying score");
 		// Here is the format of the scores: "player1Score - player2Score"
 		// "0 - 0"
 		// "15 - 15"
@@ -29,6 +131,6 @@ public class TennisGame {
 		// "game player2"
 
 		// TO BE IMPLEMENTED
-		return "";
+		return score;
 	}
 }
