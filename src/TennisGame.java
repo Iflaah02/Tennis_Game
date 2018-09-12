b547176d6afccd89f6b8b673c2cb032577fb7b4b
Nelson <nelson.lemercier@laposte.net>
2018-09-12 18:45:06 +0300

    Assignment finished

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+import javax.xml.ws.handler.MessageContext.Scope;
 
 public class TennisGame {
 	
@@ -6,29 +9,226 @@ public class TennisGame {
 	private int scoreFlag;
 	
 	public TennisGame() {
-		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
+		
+		this.p1 = 0;
+		this.p2 = 0;
+		this.scoreFlag = 0;
+		
+	}
+	
+	public void addPointPlayer(int Noplayer) {
+		
+		if(Noplayer == 1) {
+			
+			System.out.println("Player 1 scored !\n");
+			
+			if(scoreFlag == 15) {
+				
+				p2--;
+				
+			}
+			
+			else if(p1 < 5) {
+				
+				p1++;
+				
+			}
+			
+		}
+		
+		if(Noplayer == 2) {
+			
+			System.out.println("Player 2 scored !\n");
+			
+			if(scoreFlag == 14) {
+				
+				p1--;
+				
+			}
+			
+			else if(p2 < 5) {
+				
+				p2++;
+				
+			}
+			
+		}
 		
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
-		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
-		// "30 - 15", "15 - 30"
-		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
-		// "game player1"
-		// "game player2"
-
-		// TO BE IMPLEMENTED
-		return "";
+		
+		if(p1 == 0 && p2 == 0) {
+			
+			scoreFlag = 0;
+			
+		}
+		
+		if(p2 == 1 && p2 == 1) {
+			
+			scoreFlag = 1;
+			
+		}
+		
+		if(p1 == 2 && p2 == 2) {
+			
+			scoreFlag = 2;
+			
+		}
+		
+		if(p1 == 3 && p2 == 3) {
+			
+			scoreFlag = 3;
+			
+		}
+		
+		
+		if(p1 == 1 && p2 == 0) {
+			
+			scoreFlag = 4;
+			
+		}
+		
+		if(p1 == 0 && p2 == 1) {
+			
+			scoreFlag = 5;
+			
+		}
+		
+		if(p1 == 2 && p2 == 0) {
+			
+			scoreFlag = 6;
+			
+		}
+		
+		if(p1 == 0 && p2 == 2) {
+			
+			scoreFlag = 7;
+			
+		}
+		
+		if(p1 == 3 && p2 == 0) {
+			
+			scoreFlag = 8;
+			
+		}
+		
+		if(p1 == 0 && p2 == 3) {
+			
+			scoreFlag = 9;
+			
+		}
+		
+		if(p1 == 2 && p2 == 1) {
+			
+			scoreFlag = 10;
+			
+		}
+		
+		if(p1 == 1 && p2 == 2) {
+			
+			scoreFlag = 11;
+			
+		}
+		
+		if(p1 == 3 && p2 == 1) {
+			
+			scoreFlag = 12;
+			
+		}
+		
+		if(p1 == 1 && p2 == 3) {
+			
+			scoreFlag = 13;
+			
+		}
+		
+		if(p1 == 4 && p2 == 3) {
+			
+			scoreFlag = 14;
+			
+		}
+		
+		if(p1 == 3 && p2 == 4) {
+			
+			scoreFlag = 15;
+			
+		}
+		
+		if(p1 == 3 && p2 == 2) {
+			
+			scoreFlag = 18;
+			
+		}
+		if(p1 == 2 && p2 == 3) {
+			
+			scoreFlag = 19;
+			
+		}
+		
+		if(p1 == 5) {
+			
+			scoreFlag = 16;
+			
+		}
+		
+		if(p2 == 5) {
+			
+			scoreFlag = 17;
+			
+		}
+	
+	
+	
+		String result = "";
+		
+		switch (scoreFlag) {
+		
+		case 0:  result = "0 - 0";
+        		 break;
+        case 1:  result = "15 - 15";
+                 break;
+        case 2:  result = "30 - 30";
+                 break;
+        case 3:  result = "deuce";
+                 break;
+        case 4:  result = "15 - 0";
+                 break;
+        case 5:  result = "0 - 15";
+                 break;
+        case 6:  result = "30 - 0";
+                 break;
+        case 7:  result = "0 - 30";
+                 break;
+        case 8:  result = "40 - 0";
+                 break;
+        case 9:  result = "0 - 40";
+                 break;
+        case 10: result = "30 - 15";
+                 break;
+        case 11: result = "15 - 30";
+                 break;
+        case 12: result = "40 - 15";
+                 break;
+        case 13: result = "15 - 40";
+        		 break;
+        case 14: result = "advantage player1";
+        		 break;
+        case 15: result = "advantage player2";
+        		 break;
+        case 16: result = "game player1";
+        		 break;
+        case 17: result = "game player2";
+        		 break;      		 
+        case 18: result = "40 - 30";
+        		 break;
+        case 19: result = "30 - 40";
+				 break;
+        default: result = "wrong score";
+                 break;
+    }
+		
+    return result + "\n";
+    
 	}
 }
