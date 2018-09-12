bb5309dbe229f126ada01421a5e7fb67a3b948ab
Laiq <Laiq@100.67.134.24>
2018-09-12 18:26:41 +0300

    Assignmenet Finished.

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,8 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("-----Welcome---");
+		System.out.println("-----It's Wii Game---");
 		Scanner input = new Scanner(System.in);
 		
 		int finish = 0;
