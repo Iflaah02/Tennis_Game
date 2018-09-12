9d224cfb5f0e609262b72b0f9079a5bf2acf3b7c
sweiding18 <sweiding18@ts137-pc3.student.yo.oulu.fi>
2018-09-12 18:28:46 +0300

    few changes

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Driver {
 
@@ -5,6 +6,32 @@ public class Driver {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
 		play.getScore();
+		
+		Scanner scanner = new Scanner(System.in); 
+		
+		while(play.getScoreFlag() == 0){
+			System.out.println("Enter 1 for giving 'player 1' a point, enter 2 for giving 'player 2' a point");
+			
+			int player = scanner.nextInt();
+			
+			if(player == 1)
+			{
+				int newScore = play.getP1() + 1; 
+				System.out.println("Player 1 scores");
+				play.setP1(newScore);
+			}
+			else if(player == 2){
+				int newScore = play.getP2() + 1; 
+				System.out.println("Player 2 scores");
+				play.setP2(newScore); 
+			}
+			else {
+				System.out.println("Wrong input! Follow instructions.");
+			}
+			System.out.println(play.getScore()); 
+		}
+		
+		play.getScore(); 
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,34 +1,137 @@
 
 public class TennisGame {
-	
+
+
 	private int p1;
 	private int p2;
 	private int scoreFlag;
-	
+
 	public TennisGame() {
 		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
-		
+		//System.out.println("I am constructor");
+		p1 = 0; 
+		p2 = 0; 
+		scoreFlag = 0; 
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
+		// System.out.println("I am method for displaying score");
 		// Here is the format of the scores: "player1Score - player2Score"
+		
+		String score = new String(); 
+		
 		// "0 - 0"
-		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
-		// "30 - 15", "15 - 30"
-		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
-		// "game player1"
-		// "game player2"
+		if(p1 == 0 && p2 == 0) {
+			score = new String("0 - 0"); 
+		}// "15 - 15"
+		else if(p1 == 1 & p2 == 1) {
+			score = "15 - 15"; 
+		}// "30 - 30"
+		else if(p1 == 2 && p2 == 2) {
+			score = "30 - 30"; 
+		}// "deuce"
+		else if(p1 == 3 && p2 == 3) {
+			score = "deuce"; 
+		}// "15 - 0", "0 - 15"
+		else if(p1 == 1 && p2 == 0)
+		{
+			//score = new String("15 - 0"); 
+			score = "15 - 0"; 
+		}
+		else if(p1 == 0 && p2 == 1)
+		{
+			score = "0 - 15"; 
+		}// "30 - 0", "0 - 30"
+		else if(p1 == 2 && p2 == 0)
+		{
+			score = "30 - 0"; 
+		}
+		else if(p1 == 0 && p2 == 2)
+		{
+			score = "0 - 30"; 
+		}// "40 - 0", "0 - 40"
+		else if(p1 == 3 && p2 == 0)
+		{
+			score = "40 - 0"; 
+		}
+		else if(p1 == 0 && p2 == 3)
+		{
+			score = "0 - 40"; 
+		}// "30 - 15", "15 - 30"
+		else if(p1 == 2 && p2 == 1)
+		{
+			score = "30 - 15"; 
+		}
+		else if(p1 == 1 && p2 == 2)
+		{
+			score = "15 - 30"; 
+		}// "40 - 15", "15 - 40"
+		else if(p1 == 3 && p2 == 1)
+		{
+			score = "40 - 15"; 
+		}
+		else if(p1 == 1 && p2 == 3)
+		{
+			score = "15 - 40"; 
+		}// "40 - 30", "30 - 40"
+		else if(p1 == 3 && p2 == 2)
+		{
+			score = "40 - 30"; 
+		}
+		else if(p1 == 2 && p2 == 3)
+		{
+			score = "30 - 40"; 
+		}// "advantage player1"
+		else if(p1 - p2 == 1 && p1 > 3 )
+		{
+			score = "advantage player1"; 
+		}// "advantage player2"
+		else if(p2 - p1 == 1 && p2 > 3)
+		{
+			score = "advantage player2"; 
+		}// "game player1"
+		else if(p1 - p2 > 1 && p1 > 3)
+		{
+			scoreFlag = 1; 
+			score = "game player1";
+		
+		}// "game player2"
+		else if(p2 - p1 > 1 && p2 > 3)
+		{
+			score = "game player2";
+			scoreFlag = 1; 
+		}
+		else if(p1 == p2)
+		{
+			score = "deuce"; 
+		}
 
+		
 		// TO BE IMPLEMENTED
-		return "";
+		return score;
+	}
+	
+	public int getP1() {
+		return p1;
+	}
+
+	public void setP1(int p1) {
+		this.p1 = p1;
+	}
+
+	public int getP2() {
+		return p2;
+	}
+
+	public void setP2(int p2) {
+		this.p2 = p2;
+	}
+	
+	public int getScoreFlag() {
+		return scoreFlag;
+	}
+
+	public void setScoreFlag(int scoreFlag) {
+		this.scoreFlag = scoreFlag;
 	}
 }
