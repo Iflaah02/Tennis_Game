28e554d4982f076499867a4ac4e12d1c4e5b20eb
bmustone <bmustone@W10-130.student.yo.oulu.fi>
2018-09-13 15:37:19 +0300

    Assignment Finished

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		Scanner reader = new Scanner(System.in);
+		int input = 0;
+		while((!play.getScore().equals("game player1")) || (play.getScore().equals("game player2"))) {
+			System.out.println("Who wins this round? Input 1 for player 1 and 2 for player 2 >");
+			try {
+				input = reader.nextInt();
+				if((input == 1) || (input == 2)) {
+					play.winRoundForPlayer(input);
+				}else {
+					System.out.println("Not a valid player number!");
+				}
+			}catch(InputMismatchException ime) {
+				System.out.println("Not a number!");
+				reader.nextLine();
+			}
+			System.out.println("Results after this round: " + play.getScore());
+		}
+		System.out.println("Game finished.");
+		reader.close();
 	}
-
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -3,16 +3,14 @@ public class TennisGame {
 	
 	private int p1;
 	private int p2;
-	private int scoreFlag;
 	
 	public TennisGame() {
-		// TO BE IMPLEMENTED
+		p1 = 0;
+		p2 = 0;
 		System.out.println("I am constructor");
-		
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
 		// Here is the format of the scores: "player1Score - player2Score"
 		// "0 - 0"
 		// "15 - 15"
@@ -27,8 +25,48 @@ public class TennisGame {
 		// "advantage player2"
 		// "game player1"
 		// "game player2"
-
-		// TO BE IMPLEMENTED
-		return "";
+		
+		String finalScore = "";
+		
+		if((p1 >= 4 || p2 >= 4) && ((p1 - p2) >= 2)) {
+			finalScore = "game player1";
+		}else if((p1 >= 4 || p2 >= 4) && ((p2 - p1) >= 2)) {
+			finalScore = "game player2";
+		}else if((p1 >= 3 && p2 >= 3) && (p1 == p2)) {
+			finalScore = "deuce";
+		}else if((p1 >= 3 && p2 >= 3) && (p1 > p2)) {
+			finalScore = "advantage player1";
+		}else if((p1 >= 3 && p2 >= 3) && (p1 < p2)) {
+			finalScore = "advantage player2";
+		}else {
+			finalScore = (getPlayerScoreString(p1) + " - " + getPlayerScoreString(p2));
+		}
+		
+		return finalScore;
+	}
+	
+	public String getPlayerScoreString(int playerScore) {
+		String returnString = "";
+		switch(playerScore){
+			case 0: 	returnString = "0";
+						break;
+			case 1: 	returnString = "15";
+						break;
+			case 2: 	returnString = "30";
+						break;
+			case 3: 	returnString = "40";
+						break;
+			default:	returnString = "40";
+						break;
+		}
+		return returnString;
+	}
+	
+	public void winRoundForPlayer(int player) {
+		if(player == 1) {
+			p1++;
+		}else {
+			p2++;
+		}
 	}
 }
