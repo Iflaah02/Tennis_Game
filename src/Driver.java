796c730a8cadc728515b2ac5b3acbeede9c4632b
Soletek <tuukkak123@gmail.com>
2018-09-13 15:24:26 +0300

    Implement scoring system and randomized games

--- src/Driver.java
+++ src/Driver.java
@@ -2,9 +2,12 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		System.out.println(play.getScore());
+		
+		while (!play.gameHasEnded()) {
+			play.playSingleRandomMatch();
+			System.out.println(play.getScore());
+		}
 	}
-
 }
