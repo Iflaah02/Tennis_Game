676d04d7edcfaa7d885af0740d7dd442d81d0c48
mikorhon <mikorhon@W10-159.student.yo.oulu.fi>
2018-09-13 16:57:58 +0300

    jatketaan kotona

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,19 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < 4) {
+			System.out.println("" +j +" - " +k);
+			j = play.getScore();
+			play.setp1(j);
+			k = play.getScore();
+			play.setp2(k);
+			
+			i++;
+		}
 	}
-
 }
+
+
