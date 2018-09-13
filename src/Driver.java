789a5257bb9729ba8c3b1518e60ab9679e3bb11b
juhlgren <juhlgren@W10-122.student.yo.oulu.fi>
2018-09-13 15:43:51 +0300

    Version of Tennis Game that is unfinished

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,45 @@
+import java.util.Random;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
+		Random luku = new Random();
+		int whowins;
+		boolean gameover=false;
+		
+		
+		System.out.println("\nThis is an example of a randomized tennis game (not match) between player 1 and player 2 as per request\n");
+
+		int a = play.getP1();
+		int b = play.getP2();
+		
+		do {
+		whowins=luku.nextInt(2) + 1;
+		if (whowins == 1) 
+			{
+			a++;
+			play.setP1(a);
+			}
+		
+		else {
+			 b++;
+			 play.setP2(b);
+			 }
+		if (a > 5 && b < 4) gameover=true;
+		if (b < 5 && b < 4) gameover=true;
+	
+		
 		play.getScore();
+		}
+		while (gameover == false);
+		
+		
+			
+	
+		
+		
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -11,10 +11,45 @@ public class TennisGame {
 		
 	}
 
+	public void setP1 (int a){
+	p1 = a;	
+	}
+	
+	public void setP2 (int b) {
+	p2 = b;
+	}
+	
+	public int getP1 ()
+	{
+	return p1;	
+	}
+	
+	public int getP2 ()
+	{
+	return p2;
+	}
+	
+	
 	public String getScore() {
-		System.out.println("I am method for displaying score");
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
+		int a = p1;
+		int b = p2;
+		if (a == 1) a=15;
+		if (a == 2) a=30;
+		if (a == 3) a=40;
+		if (b == 1) b=15;
+		if (b == 2) b=30;
+		if (b == 3) b=40;
+		System.out.println("\nplayer1Score - player2Score\n");
+		if (p1 < 4 && p2 <4) System.out.println(a+" - "+b);
+		if (p1 == p2) System.out.println(" deuce");
+		
+		//ing advantage
+		if (p1 == 4 && p2 ==3) System.out.println("Advantage player 1");
+		if (p2 == 4 && p1 ==3) System.out.println("Advantage player 2");
+			
+		
+		if(p1 == 5) System.out.println("\nPlayer 1 wins the game\n");
+		if(p2 == 5) System.out.println("\nPlayer 2 wins the game\n");
 		// "15 - 15"
 		// "30 - 30"
 		// "deuce"
