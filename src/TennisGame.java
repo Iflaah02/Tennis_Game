5846b17aaf4fb9a7455fe5879d8d6ce01b806faf
tapantti <tapantti@W10-40.student.yo.oulu.fi>
2018-09-13 15:39:50 +0300

    commit 1 15:39

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -4,31 +4,57 @@ public class TennisGame {
 	private int p1;
 	private int p2;
 	private int scoreFlag;
-	
+	/*Who wins the next point is based on user input*/
 	public TennisGame() {
 		// TO BE IMPLEMENTED
 		System.out.println("I am constructor");
+		p1 = 0;
+		p2 = 0;
+		scoreFlag = 0;
 		
+	}
+	
+	public int getFlag() {
+		return scoreFlag;
 	}
 
 	public String getScore() {
+		String score = null;
+		int flag = this.getFlag();
+		String[] scoreArray = {"0 - 0", "15 - 0", "0 - 15", "15 - 15"};
 		System.out.println("I am method for displaying score");
 		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
+		// "0 - 0", scoreFlag 0
+		
+		// "15 - 0", "0 - 15", scoreFlag 1
+		
 		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
+		// "30 - 0", "0 - 30", scoreFlag 2
+		
 		// "30 - 15", "15 - 30"
+		// "40 - 0", "0 - 40", scoreFlag 3
+		
+		// "30 - 30"
 		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
+		// "game player1"
+		// "game player2", scoreFlag 4
+		
+		// "40 - 30", "30 - 40"
+		// "game player1"
+		// "game player2", scoreFlag 5
+		
+		// "deuce", scoreFlag 6
 		// "game player1"
 		// "game player2"
+		
+		// "advantage player1"
+		// "advantage player2", scoreFlag 7
+		
+		// "game player1"
+		// "game player2", scoreFlag 8
 
 		// TO BE IMPLEMENTED
-		return "";
+		String output = String.format("player1 - player2\n%s", score);
+		return output;
 	}
 }
