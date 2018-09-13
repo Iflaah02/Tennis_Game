4c167fa107a8993824f4ad5e1087e7527e599572
DelErr <heli.marjomaa@gmail.com>
2018-09-13 16:32:15 +0300

    test

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
 
 public class Driver {
 
+	private static TennisGame play;
+	private static Scanner input = new Scanner(System.in);
+	private static boolean driverRun = true;
+
+	private static String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TennisGame play = new TennisGame();
-		play.getScore();
+
+
+		do {
+			System.out.print("\nNew Game(N)  Quit(Q) > ");
+
+			switch (input.nextLine().toLowerCase()){
+				case "n":
+					System.out.println("Default player names");
+					play = new TennisGame();
+
+
+					break;
+				case "q":
+					driverRun = false;
+					break;
+				default:
+					System.out.println(MESSAGE_UNKNOWN_COMMAND);
+					break;
+			}
+		}while (driverRun);
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,20 +1,34 @@
 
 public class TennisGame {
 	
-	private int p1;
-	private int p2;
+	private int p1, p2;
+	private String player1="p1", player2="p2";
+	private String playerAdvantage;
 	private int scoreFlag;
 	
 	public TennisGame() {
-		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
+		//p1=0; p2=0;
+
+		System.out.println("");
 		
 	}
 
-	public String getScore() {
+	public boolean getScore() {
 		System.out.println("I am method for displaying score");
+
+		if (p1==0 && p2==0) {
+			System.out.println("love");
+		} else if ((p1>0 || p2>0) && !(p1==40 && p2==40) && (p1<=40 || p2<=40)) {
+			System.out.println(p1 +  " - " + p2);
+		}else if (p1==40 && p2==40) {
+			System.out.println("deuce");
+		}else if ((p1==40 && p2==41) || (p1==41 && p2==40)) {
+			System.out.println("advantage " + playerAdvantage);
+		}else
+			System.out.println(playerAdvantage + " wins");
+
 		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
+		// "0 - 0" "love"
 		// "15 - 15"
 		// "30 - 30"
 		// "deuce"
@@ -25,10 +39,10 @@ public class TennisGame {
 		// "40 - 15", "15 - 40"
 		// "advantage player1"
 		// "advantage player2"
-		// "game player1"
-		// "game player2"
+		// "win player1"
+		// "win player2"
 
 		// TO BE IMPLEMENTED
-		return "";
+		return true;
 	}
 }
