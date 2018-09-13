f32934caf0fd980e705c81f3b116cddb0ee7520d
rmakynen <riku.makynen@student.oulu.fi>
2018-09-13 16:11:17 +0300

    New version.

--- src/Driver.java
+++ src/Driver.java
@@ -1,16 +1,17 @@
 
-public class Driver {
-
+public class Driver
+{
 	public static void main(String[] args)
 	{
 		//Hard-coding was selected to make it easy to show that each situation can be satisfied.
 		
-		// TODO Auto-generated method stub
+		//Player 2 wins easily
 		TennisGame play = new TennisGame();
 		play.getScore();
 		play.Score(1);			//Player 1 scores
 		play.getScore();
 		play.Score(2);			//Player 2 scores
+		play.Score(3);			//Test the functoin with incorrect input. There is no player 3.
 		play.getScore();
 		play.Score(2);
 		play.getScore();
@@ -19,8 +20,7 @@ public class Driver {
 		play.Score(2);
 		play.getScore();		
 		
-		
-		
+		//Next round---------------------------------
 		play.Score(1);
 		play.getScore();
 		play.Score(2);
@@ -35,8 +35,22 @@ public class Driver {
 		play.getScore();	//Deuce 40 - 40
 		play.Score(2);
 		play.getScore();	//Advantage player 2
-		
+		play.Score(2);
+		play.getScore();	//game player 2
 
+		//player 1 wins after a long round --------------------------
+		int i=0;
+		do
+		{
+			play.Score(1);
+			play.getScore();
+			play.Score(2);
+			play.getScore();
+			i++;
+		}while(i<8);
+		play.Score(1);
+		play.getScore();
+		play.Score(1);
+		play.getScore();
 	}
-
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,11 +1,7 @@
-
 public class TennisGame
 {
-//	private int p1;
-//	private int p2;
-//	private int scoreFlag;
-	private long oneScore=0;	//9,223,372,036,854,775,807 rounds should be sufficient to figure out the winner. A potential down side to not using the flag is the fact the we have to use ~4 bytes of additional data (depends on implementation) to store the long.
-	private long twoScore=0;
+	private int oneScore=0;	//overflow is prevented in the "Score(...)" function
+	private int twoScore=0;
 	public TennisGame()
 	{
 		// TO BE IMPLEMENTED
@@ -13,7 +9,10 @@ public class TennisGame
 		this.oneScore=0;
 		this.twoScore=0;
 	}
-
+/*
+ * Function will score points for the specified player
+ * 
+ */
 	public void Score(int whoScored)
 	{
 		if (whoScored == 1)
@@ -24,9 +23,25 @@ public class TennisGame
 		{
 			this.twoScore=this.twoScore+1;
 		}
+		else
+		{
+			System.out.println("WARNING: Incorrect input. You can only score for player \"1\" or  \"2\".");
+		}
+		
+		//Overflow prevention for long matches:
+		if(this.oneScore > 6 && this.twoScore>6)
+		{
+			//Subtract one from each
+			this.oneScore=this.oneScore-1;
+			this.twoScore=this.twoScore-1;
+		}
 	}
 // -------------------------------------------------------------------
-	private String getScoreAmount (long value)
+	/*
+	 * getScoreAmount	 is a helper function. It transforms
+	 * a long value between 0-3 into a tennis score 0-40 
+	 */
+	private String getScoreAmount (int value)
 	{
 		String returnVal="0";
 		if(value==1)
@@ -38,46 +53,37 @@ public class TennisGame
 	return(returnVal);
 	}
 // -------------------------------------------------------------------
-	public String getScore()
+	public void getScore()
 	{
-	//System.out.println("I am method for displaying score");
-	//See if we have a winner
-	long difference = Math.abs(this.oneScore - this.twoScore);
-	
-
-	System.out.println("----------------------------------");
-	System.out.println("difference: "+ difference);
-//	System.out.println("one: "+ this.oneScore);
-//	System.out.println("one: "+ this.twoScore);
-	
-	
-	if(difference >= 2 && (this.oneScore >= 4 || this.twoScore >= 4))
-	{
-		if (this.oneScore > this.twoScore)
+		//See if we have a winner
+		int difference = Math.abs(this.oneScore - this.twoScore);	//How many points is one player leading by
+		if(difference >= 2 && (this.oneScore >= 4 || this.twoScore >= 4))
 		{
-			System.out.println("game Player 1");
+			if (this.oneScore > this.twoScore)
+			{
+				System.out.println("game Player 1");
+			}
+			else			
+			{
+				System.out.println("game Player 2");
+			}
+			System.out.println("-----------------------------------");
+			//Round is done. Reset scores for the next round.
+			this.oneScore=0;
+			this.twoScore=0;
 		}
-		else			
+		//No winners for the round. Let's show the score:
+		else if (this.oneScore <=3 && this.twoScore <=3)
 		{
-			System.out.println("game Player 2");
+			if(this.oneScore == 3 && this.twoScore==3)
+				System.out.println("deuce");
+			else
+				System.out.println(this.getScoreAmount(this.oneScore)+" - " +this.getScoreAmount(this.twoScore));
 		}
-		System.out.println("-----------------------------------");
-		//Round is done. Let's reset the scores for the next round.
-		this.oneScore=0;
-		this.twoScore=0;
-	}
-	//No winners for the round. Let's show the score:
-	else if (this.oneScore <=3 && this.twoScore <=3)
-	{
-		if(this.oneScore == 3 && this.twoScore==3)
-			System.out.println("deuce");
+		//One player has advantage:
 		else
-			System.out.println(this.getScoreAmount(this.oneScore)+" - " +this.getScoreAmount(this.twoScore));
-	}
-	//One player has advantage:
-	else
-	{
-			if(difference<2)
+		{
+			if(difference<2 && difference > 0)
 			{
 				if(this.oneScore > this.twoScore)
 				{
@@ -92,9 +98,8 @@ public class TennisGame
 			{
 				System.out.println("deuce");
 			}
-		
-	}
-
+			
+		}
 		// Here is the format of the scores: "player1Score - player2Score"
 		// "0 - 0"
 		// "15 - 15"
@@ -109,8 +114,5 @@ public class TennisGame
 		// "advantage player2"
 		// "game player1"
 		// "game player2"
-
-		// TO BE IMPLEMENTED
-		return "";
 	}
 }
