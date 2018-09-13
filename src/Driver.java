37c5a6de178034d598b60274e96e855ebd0495a8
SlowNStupid <36250194+SlowNStupid@users.noreply.github.com>
2018-09-13 23:58:57 +0300

    Assignment Finished

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,25 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+        Random random = new Random();
+
+		while (!play.isSetOver()) {
+		    play.setScore(random.nextInt(2) + 1);
+		    System.out.println(play.getScore());
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(play.getSetDetails());
 	}
 
 }
