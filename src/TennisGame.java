2bcf27ef27c0d684778edd3cea33787348ca4502
Bruno Triani <btriani@gmail.com>
2018-09-13 15:39:56 +0300

    at 15:39

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
+	private char oneLetter;
 	private int scoreFlag;
+	private boolean GameON;
 	
 	public TennisGame() {
 		// TO BE IMPLEMENTED
@@ -16,8 +19,39 @@ public class TennisGame {
 		System.out.println("I am method for displaying score");
 		p1 = 0;
 		p2 = 0;
+		GameON = true;
 		
-		System.out.println("Beggining of the game -> p1= " +p1+ " vs p2= "+p2);
+		System.out.println("Beggining of the GAME -> A = " +p1+ " vs B = "+p2);
+
+
+		while (GameON = true) 
+		{
+			Scanner reader = new Scanner(System.in);  // Reading from System.in
+			System.out.println("Enter a player that scores next: ");
+
+			Scanner in = new Scanner (System.in);
+			char oneLetter = reader.next().charAt(0);
+			reader.close();
+			
+			if (oneLetter!='A') 
+			{
+				p1 += 15;
+				System.out.println("GAME 1 -> A = " +p1+ " vs B = "+p2);
+			} 
+			
+			if (oneLetter!='B') 
+			{
+				p2 += 15;
+				System.out.println("GAME 2 -> A = " +p1+ " vs B = "+p2);
+			} 
+			
+			//GameON = false;
+			
+		}
+					
+				
+			
+		
 		
 		
 		// Here is the format of the scores: "player1Score - player2Score"
