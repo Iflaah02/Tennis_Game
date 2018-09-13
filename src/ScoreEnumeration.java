8c50b684b2bf80c4729a360e6c658fdceae4bbe8
Salonurmi Petteri <Petteri.Salonurmi@fcg.fi>
2018-09-13 15:10:16 +0300

    ScoreEnumeration.java: * Added more jDoc * Cleaned up code smells

--- src/ScoreEnumeration.java
+++ src/ScoreEnumeration.java
@@ -1,9 +1,13 @@
 
 /**
  * public class scoreEnumeration
- *
  * An enumeration class for the score values.
  *
+ * Love - 0 points
+ * Fifteen - 15 points
+ * Thirty - 30 points
+ * Forty - 40 points
+ * Advance - The player is about to win
  */
 public enum ScoreEnumeration {
 
