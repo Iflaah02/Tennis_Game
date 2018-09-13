2ace4be57c99b7336a285171f095dc7e494fbe0b
DelErr <heli.marjomaa@gmail.com>
2018-09-14 01:30:21 +0300

    done

--- src/Driver.java
+++ src/Driver.java
@@ -7,29 +7,73 @@ public class Driver {
 	private static boolean driverRun = true;
 
 	private static String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+	private static String MESSAGE_TRY_AGAIN = "Please try again.";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 
 		do {
 			System.out.print("\nNew Game(N)  Quit(Q) > ");
 
 			switch (input.nextLine().toLowerCase()){
 				case "n":
-					System.out.println("Default player names");
 					play = new TennisGame();
-
-
+					gameOn();
+					input.nextLine();
 					break;
 				case "q":
 					driverRun = false;
 					break;
 				default:
 					System.out.println(MESSAGE_UNKNOWN_COMMAND);
+					System.out.println(MESSAGE_TRY_AGAIN);
 					break;
 			}
 		}while (driverRun);
+	}
+
+	private static void gameOn (){
+
+		boolean win;
+
+		play.getScore();
+
+		do {
+			System.out.println("Which wins the set, player1(1) or player2(2)?");
+			play.update(checkInput());
+
+			win = play.getScore();
+
+		}while (!win);
+
+
+	}
+
+	private static int checkInput (){
+
+		String MESSAGE_ENTER_1_OR_2 = "Please enter either 1 or 2";
+		int player;
+
+		while (true) {
+			try {
+
+				player = Integer.parseInt(input.next());
+
+				if (player == 1 || player == 2) {
+					return player;
+				}
+
+				System.out.println(MESSAGE_UNKNOWN_COMMAND);
+				System.out.println(MESSAGE_ENTER_1_OR_2);
+
+			} catch (NumberFormatException ignore) {
+				System.out.println(MESSAGE_UNKNOWN_COMMAND);
+				System.out.println(MESSAGE_ENTER_1_OR_2);
+			}// try
+
+		}//while loop
+
+
+
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,48 +1,78 @@
 
 public class TennisGame {
 	
-	private int p1, p2;
-	private String player1="p1", player2="p2";
-	private String playerAdvantage;
-	private int scoreFlag;
+	private int p1, p2; 							//player scores
+	private String player1, player2;				//player score display
+	private String playerAdvantage;					//player who is winning
 	
 	public TennisGame() {
-		//p1=0; p2=0;
-
-		System.out.println("");
+		p1=0;
+		p2=0;
+		player1 = "love";
+		player2 = "love";
 		
 	}
 
 	public boolean getScore() {
-		System.out.println("I am method for displaying score");
 
 		if (p1==0 && p2==0) {
-			System.out.println("love");
-		} else if ((p1>0 || p2>0) && !(p1==40 && p2==40) && (p1<=40 || p2<=40)) {
-			System.out.println(p1 +  " - " + p2);
-		}else if (p1==40 && p2==40) {
+			System.out.println("love - love");
+			return false;
+		}else if (p1==3 && p2 ==3) {
 			System.out.println("deuce");
-		}else if ((p1==40 && p2==41) || (p1==41 && p2==40)) {
-			System.out.println("advantage " + playerAdvantage);
+			return false;
+		}else if ((p1==3 && p2==4) || (p1==4 && p2==3)) {
+			System.out.println("adv " + playerAdvantage);
+			return false;
+		} else if (p1<4 && p2<4){
+			System.out.println(player1 +  " - " + player2);
+			return false;
 		}else
 			System.out.println(playerAdvantage + " wins");
-
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0" "love"
-		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
-		// "30 - 15", "15 - 30"
-		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
-		// "win player1"
-		// "win player2"
-
-		// TO BE IMPLEMENTED
 		return true;
-	}
+	} //getScore
+
+	public void update(int player) {
+
+		if (player == 1){
+
+			if (p2==4){
+				p2--;
+
+			}else if (p1 < 5){
+				p1++;
+
+				if (p1==1){
+					player1 = "15";
+				}else if (p1==2){
+					player1 = "30";
+				}else if (p1==3){
+					player1 = "40";
+				}else if (p1==4){
+					playerAdvantage = "p1";
+				}
+			} // if player == 1
+
+		} else /*player == 2*/{
+
+			if (p1==4){
+				p1--;
+			} else if (p2 < 5) {
+				p2++;
+
+				if (p2==1){
+					player2 = "15";
+				}else if (p2==2){
+					player2 = "30";
+				}else if (p2==3){
+					player2 = "40";
+				}else if (p2==4){
+					playerAdvantage = "p2";
+				}
+			}
+		} // if player == 2
+
+
+
+	} //update
 }
