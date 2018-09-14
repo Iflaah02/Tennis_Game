7df3b9830eb48c7798858cf60a77a43e88d83caf
Pepetti <pirkkapekka@gmail.com>
2018-09-14 21:17:37 +0300

    Assignment Finished

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner reader = new Scanner (System.in);
+		int ip = 0;
+		
 		TennisGame play = new TennisGame();
-		play.getScore();
+		
+		while (play.p1_getScoreFlag() != 4 && play.p2_getScoreFlag() != 4) {
+			try {
+				play.getScore();
+				ip = reader.nextInt();
+				reader.nextLine();
+				play.gameLogic(ip);
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Use number 1 or 2");
+				reader.nextLine();
+				continue;
+			}
+		}
+		reader.close();
 	}
 
 }
