2588595791ae55043ff1fbba4160e3176f4fa9a3
Tapio <tapio-a@hotmail.com>
2018-09-14 22:35:14 +0300

    Assignment Finished

--- src/Driver.java
+++ src/Driver.java
@@ -2,9 +2,8 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		//play.getScore();
 	}
 
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,75 +1,70 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
-	private int scoreFlag;
-	/*Who wins the next point is based on user input*/
+	//private int scoreFlag;
+	Scanner sc = new Scanner(System.in);
+
 	public TennisGame() {
-		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
+		//System.out.println("I am constructor");
 		p1 = 0;
 		p2 = 0;
-		scoreFlag = 0;
+		//scoreFlag = 0;
+		System.out.println(this.getScore());
+		while (!this.getScore().contains("game")) {
+			int pId;
+			do {
+				System.out.println("Type the number of the player who wins the point. Only numbers 1 & 2 are allowed.\n(1 = Player 1, 2 = Player 2) >");
+				while (!sc.hasNextInt()) {
+					System.out.println("Invalid input!");
+					System.out.println("Type the number of the player who wins the point. Only numbers 1 & 2 are allowed.\n(1 = Player 1, 2 = Player 2) >");
+					sc.next();
+				}
+				pId = sc.nextInt();
+			} while (pId<1 || pId>2);
+			
+			if (pId == 1) {
+				p1 += 1;
+			}
+			else {
+				p2 += 1;
+			}
+			System.out.println(this.getScore());
+		}
 		
-	}
-	
-	public int getFlag() {
-		return scoreFlag;
 	}
 
 	public String getScore() {
-		String score = null;
-		int flag = this.getFlag();
-		List<String> pointsDescription = Arrays.asList("love", "fifteen", "thirty", "forty");
-		System.out.println("I am method for displaying score");
-
-	        if (p1 >= 3 && p2 >= 3) {
+		List<String> pointsDescription = Arrays.asList("0", "15", "30", "40");
+		//System.out.println("I am method for displaying score");
+			
+			if (p1 > 3 && p2 <= 2) {
+				return "game player1";
+			}
+			else if (p2 > 3 && p1 <= 2) {
+				return "game player2";
+			}
+			else if (p1 >= 3 && p2 >= 3) {
 	            if (p2 - p1 >= 2) {
-	                return getLeadPlayer().getName() + " won"; //!!
+	                return "game player2";
+	            } else if (p1 - p2 >= 2) {
+	            	return "game player1";
 	            } else if (p1 == p2) {
 	                return "deuce";
 	            } else {
-	                return "advantage " + getLeadPlayer().getName(); //!!
+	            	String inAdv = (p1 > p2) ? "player1" : "player2";
+	                return "advantage " +  inAdv;
 	            }
 	        } else {
-	            return player1.getScoreDescription() + ", " + player2.getScoreDescription(); //!!
+	            return "Score: Player 1 - Player 2\n"
+	            		+ pointsDescription.get(p1)
+	            		+ " - "
+	            		+ pointsDescription.get(p2);
 	        }
-	    
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0", scoreFlag 0
-		
-		// "15 - 0", "0 - 15", scoreFlag 1
-		
-		// "15 - 15"
-		// "30 - 0", "0 - 30", scoreFlag 2
-		
-		// "30 - 15", "15 - 30"
-		// "40 - 0", "0 - 40", scoreFlag 3
-		
-		// "30 - 30"
-		// "40 - 15", "15 - 40"
-		// "game player1"
-		// "game player2", scoreFlag 4
-		
-		// "40 - 30", "30 - 40"
-		// "game player1"
-		// "game player2", scoreFlag 5
-		
-		// "deuce", scoreFlag 6
-		// "game player1"
-		// "game player2"
-		
-		// "advantage player1"
-		// "advantage player2", scoreFlag 7
-		
-		// "game player1"
-		// "game player2", scoreFlag 8
 
-		// TO BE IMPLEMENTED
-		String output = String.format("player1 - player2\n%s", score);
-		return output;
 	}
 }
