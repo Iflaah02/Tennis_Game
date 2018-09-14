26879751ee4af1de105cc14582d3573b82001376
Janne Annunen <jannunen@edu.oulu.fi>
2018-09-14 09:09:16 +0300

    20180914 almost done

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,41 @@
-
+import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TennisGame play = new TennisGame();
-		play.getScore();
-	}
+		boolean running = true;
+		Scanner scan= new Scanner(System.in);
 
+	    //For string
+
+	    int readed;
+
+	    //System.out.println(text);
+
+	    int a =0;
+	  
+	    
+		TennisGame play = new TennisGame();
+		while(running) {
+		
+			readed= scan.nextInt();
+			System.out.println(readed);
+		if (readed == 1) {
+			play.p1s();
+			System.out.println(play.getScore());
+			
+		}
+		if (readed == 2) {
+			play.p2s();
+			System.out.println(play.getScore());
+			
+		}
+		if (readed == 0) {
+			running = false;
+			System.out.println("Game end");
+		}
+		
+		
+		}
+	}
 }
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,34 +1,99 @@
-
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
-	private int scoreFlag;
+	private int scoreFlag = 0;
 	
 	public TennisGame() {
 		// TO BE IMPLEMENTED
-		System.out.println("I am constructor");
+		p1 = 0;
+		p2 = 0;
+		System.out.println("1 = point to player1");
+		System.out.println("2 = point to player2");
+		System.out.println("0 = End Game");
 		
 	}
 
 	public String getScore() {
-		System.out.println("I am method for displaying score");
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
-		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
-		// "30 - 15", "15 - 30"
-		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
-		// "game player1"
-		// "game player2"
+		//System.out.println("I am method for displaying score");
 
-		// TO BE IMPLEMENTED
-		return "";
+
+		if(scoreFlag == 1) {
+			resetPoints();
+			scoreFlag=0;		
+		}
+		
+		if (hasWinner()) {
+			scoreFlag = 1;
+			return playerWithHighestScore() + " wins. Press any key and Enter to start new game";
+		}
+		
+		if (hasAdvantage()) { 
+			return "Advantage " + playerWithHighestScore(); 
+		}
+		
+		if (isDeuce()) {
+			return "Deuce";
+		}
+				
+		return translateScore(p1) + " - " + translateScore(p2);
+		
+		
+	}
+	private boolean isDeuce() {
+		return p1 >= 3 && p2 == p1;
+	}
+	
+	private String playerWithHighestScore() {
+		if (p1 > p2) {
+			return "player1";
+		} else {
+			return "player2";
+		}
+	}
+	
+	private boolean hasWinner() {
+		if(p2 >= 4 && p2 >= p1 + 2 )
+			return true;
+		if(p1 >= 4 && p1 >= p2 + 2)
+			return true;
+		return false;
+	}
+	
+	private boolean hasAdvantage() {
+		if (p2 >= 4 && p2 == p1 + 1)
+			return true;
+		if (p1 >= 4 && p1 == p2 + 1)
+			return true;
+		
+		return false;
+
+	}
+
+	public void p1s() {
+		p1++;
+	}
+
+	public void p2s() {
+		p2++;
+	}
+	
+	public void resetPoints() {
+		p1=0;
+		p2=0;
+	}
+
+	private String translateScore(int score) {
+		switch (score) {
+		case 3:
+			return "40";
+		case 2:
+			return "30";
+		case 1: 
+			return "15";
+		case 0:
+			return "Love";
+		}
+		throw new IllegalArgumentException("Illegal score: " + score);
 	}
 }
