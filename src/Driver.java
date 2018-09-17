8a4ed3fac3817434ca22a7adc63f1f8d5e2105e6
Kalle Ollonqvist <ollonqistkalle@gmail.com>
2018-09-17 22:55:49 +0300

    Assignment Finished

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,14 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TennisGame play = new TennisGame();
-		play.getScore();
+		System.out.print(play.getScore()+"\n");
+		while(true) {
+			play.setScore(play.getp1score(),play.getp2score());
+			System.out.println("\n"+play.getScore());
+			if(play.getGameFlag() == 1) {
+				break;
+			}
+		}
 	}
 
 }
